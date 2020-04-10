f59127f3fbca9ea5550ffd1c4e49f90a3dd8235e
root <kmeans.shong@gmail.com>
2020-04-10 11:12:16 +0800

    # PERFECT frameworks

--- Triple_Filter.py
+++ Triple_Filter.py
@@ -14,19 +14,32 @@ params = type('obj', (object,), {"MACD_range": "12", "RSI_range": "12", "EMA_ran
                                  "BOLL_range": "20", "middle_range": PERIOD_M15, "long_range": PERIOD_M75})
 # 多头代号1，空头代号0
 
+def calculate_limit():
+    # 计算止盈止损price_limit
+    pass
+
 def manage_account():
     # 仓位管理功能，计算开仓数量、资金划转
     pass
 
 def account_station():
     # 如果持有仓位，持续观察价格变化是否达到止盈止损
+
+    # 多头止盈 -- 15m线的KDJ读数超过70
+    # 多头止损 -- 止损位置一： 交易当天或前一天的低价
+    #            止损位置二： 损益两平的价格
+    #            止损位置三、四。。。： 保护50%的账面获利
+    # 空头止盈 -- 15m线的KDJ读数小于20
+    # 空头止损 -- 止损位置一： 交易当天或前一天的最高价
+    #            止损位置二： 损益两平的价格
+    #            止损位置三、四。。。： 保护50%的账面获利
     pass
 
 def trade_chance(direction):
     # 根据RSI获取中介时间节点的入场机会
 
     # 设置期货杠杆大小、合约类型（永续）
-    exchange.SetMarginLevel(10)
+    exchange.SetMarginLevel(2)
     exchange.SetContractType("swap")
     while True:
         r = exchange.GetRecords(PERIOD_M15)
@@ -49,34 +62,42 @@ def trade_chance(direction):
                 Log("[+] 空头开仓成功： ", id)
                 break
         Sleep(10)
+    price_limit = calculate_limit()
+    return price_limit
 
 def triple_net(recoders):
     # 判断长期趋势 -- macd和ema
     macd = TA.MACD(recoders, 12)
     if recoders and len(recoders) > 22:
         ema = TA.EMA(recoders, 22)
-        if ema: # EMA多趋势判断标准
-            Log("[EMA趋势]： 向上")
+        if ema[-1] > ema[-2] > ema[-3] and (ema[-1] - ema[-3])/ema[-1] > 0.02: # EMA多趋势判断标准
+            Log("[EMA趋势]： 向上，启用脉冲交易系统")
             if macd: # MACD多趋势判断标准
                 Log("[MACD趋势]： 向上, 与EMA趋势判断相同！")
                 Log("[== 开始寻找多头入场机会 ==]")
-                trade_chance(1)
+                price_limit = trade_chance(1)
             else:
                 Log("【-- MACD趋势判断与EMA相悖，等待机会 --】")
+                price_limit = -1
 
-        if ema: # EMA空趋势判断标准
-            Log("[EMA趋势]： 向下")
+        if ema[-1] < ema[-2] < ema[-3] and (ema[-3] - ema[-1])/ema[-3] > 0.02: # EMA空趋势判断标准
+            Log("[EMA趋势]： 向下，启用脉冲交易系统")
             if macd: # MACD空趋势判断标准
                 Log("[MACD趋势]： 向下，与EMA趋势判断相同！")
                 Log("[== 开始寻找空头入场机会 ==]")
                 trade_chance(0)
+                price_limit = calculate_limit()
             else:
                 Log("【-- MACD趋势判断与EMA相悖，等待中 --】")
+                price_limit = -1
         else:
-            Log("[-- EMA多空难以判断，等待中 --]")
+            Log("[-- EMA处于震荡行情之中，启用震荡指标 --]")
+            # BOLL下轨开多，上轨开空
+            price_limit = calculate_limit()
     else:
         Log("[-- 数据量不足以计算EMA数值，等待中 --]")
-
+        price_limit = -1
+    return price_limit
 
 while True:
     # 获取持仓信息
