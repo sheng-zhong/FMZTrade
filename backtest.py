0fd5ea13af83867905e7055368c68c66cc7d0734
root <kmeans.shong@gmail.com>
2020-04-21 18:39:19 +0800

    #ADD backtest codes

--- backtest.py
+++ backtest.py
@@ -14,14 +14,27 @@ class BackTest(object):
         self.leverage = leverage
         self.commission = commission
         self.initial_balance = initial_balance
-        self.account = {"balance": self.initial_balance, ""}
-        self.trading_log =  {"open_price": [], ""}
+        self.account = {"balance": self.initial_balance, "amount": 0, "profit": 0, "commission": 0}
+        self.trading_log =  {"open_price": [], "direction": [], "close_price": [], "amount": [],
+                             "profit": [], "commission": [], "margin": []}
 
-    def update_account(self):
-        pass
+    def update_account(self, price, direction, using_margin, amount, commissions, profit=0):
+        self.account['balance'] = self.account['balance'] - using_margin - commissions
+        self.account['amount'] = amount
+        self.account['profit'] += profit
+        self.account['commission'] += commissions
+        self.trading_log['open_price'].append(price)
+        self.trading_log['direction'].append(direction)
+        self.trading_log['amount'].append(amount)
+        self.trading_log['commission'].append(commissions)
 
-    def OpenBuy(self, price, ammount):
-        pass
+    def OpenBuy(self, price, amount):
+        using_margin = price * amount / self.leverage
+        if self.account['balance'] >= using_margin:
+            commissions = price * amount * self.commission
+            self.update_account(price, direction, using_margin, amount, commissions)
+        else:
+            print("[-- 保证金不足，无法开仓 --]")
 
     def CloseBuy(self):
         pass
